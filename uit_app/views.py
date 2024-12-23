707a4b08951b3a1a28f041a165a481b90e3c2ea7
project <akhilgkrishnan102@gmail.com>
2024-12-23 19:28:53 +0530

    index page done

---
+++ uit_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+
+    template_name="index.html"
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,self.template_name)
